acf71cf36b23656e729e80c1345e5080de2a5ee5
javiiifp <jfp135712@gmail.com>
2026-04-26 20:21:14 +0200

    zoomout

--- js/main.js
+++ js/main.js
@@ -6,8 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const iconoAmpliar = `<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M15 3h6v6" /><path d="M9 21H3v-6" /><path d="M15 21h6v-6" /><path d="M9 3H3v6" /></svg>`;
     
-    const iconoReducir = `<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M4 14h6v6"/><path d="M20 10h-6V4"/><path d="M14 10h6v-6"/><path d="M10 14H4v6"/></svg>`;
-
+    const iconoReducir = `<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <polyline points="4 14 10 14 10 20"></polyline>
+        <polyline points="20 10 14 10 14 4"></polyline>
+        <line x1="14" y1="10" x2="21" y2="3"></line>
+        <line x1="10" y1="14" x2="3" y2="21"></line>
+    </svg>`;
+    
     const pageFlip = new St.PageFlip(bookElement, {
         width: 350,           
         height: 495,          
