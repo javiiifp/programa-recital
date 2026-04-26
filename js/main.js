350cba8b6d27fde2da27d25ab99aed233f68e6eb
javiiifp <jfp135712@gmail.com>
2026-04-26 17:40:04 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const bookElement = document.getElementById('book');
+    
+    const pageFlip = new St.PageFlip(bookElement, {
+        width: 400,
+        height: 600,
+        size: "stretch",
+        minWidth: 300,
+        maxWidth: 1000,
+        minHeight: 450,
+        maxHeight: 1500,
+        maxShadowOpacity: 0.5,
+        showCover: true,
+        mobileScrollSupport: true
+    });
+
+    pageFlip.loadFromHTML(document.querySelectorAll('.page'));
+
+    window.addEventListener('resize', () => {
+        pageFlip.updateFromHtml(document.querySelectorAll('.page'));
+    });
+});
